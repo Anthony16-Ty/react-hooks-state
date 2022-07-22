1b0eaec9e21181359eb02636c08280046b4b2304
Anthony Kimeu <tkymeu@gmail.com>
2022-07-22 16:01:02 +0300

    Done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 
 function Counter() {
+  // if (props.shouldHaveCount) {
+  //   // This is wrong -- never call a hook inside a condition
+  //   const [count, setCount] = useState(0);
+
+  //   // return ...
+  // }
+  // // return ...
   const [count, setCount] = useState(0);
 
   function increment() {
@@ -9,5 +16,6 @@ function Counter() {
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
 }
+
 
 export default Counter;
